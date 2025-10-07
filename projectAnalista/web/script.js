f25b439a4859c14b754018401bb2f3c7925285ac
j0v3 <201791647+j0v3@users.noreply.github.com>
2025-10-07 15:09:46 -0300

    Atualiza Dados

--- projectAnalista/web/script.js
+++ projectAnalista/web/script.js
@@ -6,6 +6,23 @@ botaClick.addEventListener("click",()=>{
 
    const dados = {nome:recNome.value, feito:recFeito.value};
    
+   fetch('../php/requests.php', {
+
+    method:'POST', 
+    headers:{
+
+        'Content-Type': 'application/json' }, 
+        body: JSON.stringify(dados)
+   }).then(response=>{
+
+        if(!response.ok){
+            throw new error("Erro na requisição: " + response.status);
+        }return response.json();
+
+   }).then(data=> {console.log("Resposta do Servidor: ", data);}).catch(error=>{
+    console.log("Erro: ", error);
+   })
+
    
 
 });
