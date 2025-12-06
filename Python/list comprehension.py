587db6fdbb0da823d1b20e0250ba11447e540970
j0v3 <201791647+j0v3@users.noreply.github.com>
2025-12-06 16:22:40 -0300

    trainning

---
+++ Python/list comprehension.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 7, 9, 10, 11, 12, 13, 14, 15] 
+numBers = [x for x in range(301)]
+
+
+def comprehesion(listinput):
+    newList = [ x for x in listinput if x % 2 != 0 ]
+    return newList
+
+newist = []
+
+for x in numbers:
+    if x % 2 == 0:
+        newist.append(x)
+
+print(comprehesion(numbers))
+print(f"Lista utilizando a função com list comprehension: {comprehesion(numBers)}")
+print(f"Lista usando for {newist}")
